113972c88c6441463f1f55b10efbf7223b749a6e
juzcallmekaushik <76829835+juzcallmekaushik@users.noreply.github.com>
2025-02-26 09:15:50 +0800

    Addition of AceTrack onto Portfolio

--- components/projects/project-item.jsx
+++ components/projects/project-item.jsx
@@ -6,26 +6,28 @@ function ProjectItem(props) {
     const { name, description, href, libs } = props;
     return (
         <a
-            href={href}
-            target="_blank"
-            rel="noopener noreferrer"
+            href={href || undefined}
+            target={href ? "_blank" : undefined}
+            rel={href ? "noopener noreferrer" : undefined}
             className="group flex flex-col p-5 transition-all bg-surface-200 hover:scale-[1.03] brightness-75 hover:brightness-100 hover:z-10 rounded-md"
         >
             <div className="text-surface-100 flex flex-row items-center justify-between mb-0"></div>
             <div className="flex items-center gap-2 mb-4">
                 <h1 className="text-xl subpixel-antialiased">{name}</h1>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="gray"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="h-4 w-4 transition-all duration-300 group-hover:-translate-y-1 group-hover:stroke-primary-600"
-                >
-                    <path d="M7 17L17 7M7 7h10v10" />
-                </svg>
+                {href && (
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="gray"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="h-4 w-4 transition-all duration-300 group-hover:-translate-y-1 group-hover:stroke-primary-600"
+                    >
+                        <path d="M7 17L17 7M7 7h10v10" />
+                    </svg>
+                )}
             </div>
             <div className="text-surface-600 text-xs mb-5">{description}</div>
             <div className="flex flex-wrap items-center gap-x-2 gap-y-2 max-w-[500px]">
